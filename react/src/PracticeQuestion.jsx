9c0ee5231258c30405c78aaa61124d0c29af8024
Ammad khan <ak5321515@gmail.com>
2024-06-06 19:18:49 +0500

    Update PracticeQuestion.jsx

--- react/src/PracticeQuestion.jsx
+++ react/src/PracticeQuestion.jsx
@@ -3,7 +3,7 @@ function PracticeQuestions(){
         <>
             <div onClick = {() =>{alert('You click the parent div')}} style={{border:"1px solid black", marginTop:"20px"}}>
                 Parent div
-                <div onClick = {(e) =>{e.stopPropagation(),alert('You click the child div')}} style={{border:"1px solid red"}}>
+                <div onClick = {(e) =>{e.stopPropagation();alert('You click the child div')}} style={{border:"1px solid red"}}>
                     child div
 
                 </div>
